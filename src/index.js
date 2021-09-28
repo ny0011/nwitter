6d853653e53a89c6b6b9b2ee0259bdcd9091e232
ny0011 <nayun011@gmail.com>
2021-09-28 16:57:27 +0900

    1. setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import firebase from "./firebase";
+
+console.log(firebase);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
