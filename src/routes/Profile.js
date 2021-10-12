96152b55f853bdb1e785f84730d8cfac0187df17
ny0011 <nayun011@gmail.com>
2021-10-12 18:26:36 +0900

    show my nweets on profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,39 @@
 import { updateProfile } from "@firebase/auth";
-import { auth } from "fbase";
-import React, { useState } from "react";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "@firebase/firestore";
+import Nweet from "components/Nweet";
+import { auth, db } from "fbase";
+import React, { useCallback, useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
 const Profile = ({ refreshUser, userObj }) => {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const [nweets, setNweets] = useState([]);
   const onLogOutClick = () => {
     auth.signOut();
     history.push("/");
   };
+
+  const getMyNweets = useCallback(async () => {
+    const q = query(
+      collection(db, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+
+    const querySnapshot = await getDocs(q);
+    setNweets(querySnapshot.docs.map((doc) => doc.data()));
+  }, [userObj]);
+
+  useEffect(() => {
+    getMyNweets();
+  }, [getMyNweets]);
 
   const onChange = (event) => {
     const {
@@ -40,6 +64,15 @@ const Profile = ({ refreshUser, userObj }) => {
         />
         <input type="submit" value="Update Profile" />
       </form>
+      <div>
+        {nweets.map((nweet, index) => (
+          <Nweet
+            key={index}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
